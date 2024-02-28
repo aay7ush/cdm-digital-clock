4a82e32604188894534056d8c0b2d4c573e4a166
aay7ush <aay7ush@gmail.com>
2024-02-28 16:45:28 +0530

    add starter files

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,8 @@
-import { Quote } from 'lucide-react'
-import { useEffect, useState } from 'react'
-import DigitalClock from './components/DigitalClock'
-
 const App = () => {
-  const [backgroundImage, setBackgroundImage] = useState('')
-
-  useEffect(() => {
-    const currentHour = new Date().getHours()
-
-    if (currentHour >= 5 && currentHour < 12) {
-      setBackgroundImage('/morning.webp')
-    } else if (currentHour >= 12 && currentHour < 17) {
-      setBackgroundImage('/afternoon.webp')
-    } else if (currentHour >= 17 && currentHour < 21) {
-      setBackgroundImage('/evening.webp')
-    } else {
-      setBackgroundImage('/night.webp')
-    }
-  }, [])
-
   return (
-    <main style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <div className="bg-mask" />
-      <article className="p-10 lg:p-20 text-white min-h-screen grid content-between text-2xl relative z-20">
-        <Quote />
-        <DigitalClock />
-      </article>
-    </main>
+    <h1 className="text-3xl font-bold underline">
+      Let's build Digital Clock App
+    </h1>
   )
 }
-
 export default App
